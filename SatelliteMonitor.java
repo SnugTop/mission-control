f3d744db79e8819cd527733114febe3b7c0b56aa
Cameron D <camjdaly@gmail.com>
2024-10-06 20:26:16 -0400

    Just getting set up. Set up files.

---
+++ SatelliteMonitor.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.test.*;
+
+
+
+public class SatelliteMonitor{
+
+
+    public static void main(String[] args) {
+        try{
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+    }
+
+}
